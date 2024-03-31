b0f4e69631c9de33cf528172a30efa4cc5af52d9
Jules Sandoz <jules.sandoz@heig-vd.ch>
2024-03-31 13:31:25 -0600

    create BasePill component

---
+++ components/BasePill.vue
@@ -0,0 +1,47 @@
+<template>
+  <input
+    type="checkbox"
+    :name="props.text"
+    :id="props.text"
+    @change="$emit('pillChecked')"
+  />
+  <label :for="props.text" class="pill">{{ props.text }}</label>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  text: string;
+}>();
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/variables.scss";
+
+.pill {
+  border: solid 2px $fontColor;
+  border-radius: 20px;
+  background-color: $bgColor;
+  padding: 0.5em 1em;
+  color: $fontColor;
+  margin: 0.5em;
+  cursor: pointer;
+
+  &:hover {
+    background-color: $bgColorHover;
+    color: $fontColor;
+  }
+}
+
+input[type="checkbox"] {
+  display: none;
+}
+
+input[type="checkbox"]:checked + label {
+  background-color: $fontColor;
+  color: $bgColor;
+
+  &:hover {
+    background-color: $fontColorHover;
+  }
+}
+</style>
