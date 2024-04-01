fc2b263395b51bff919d5d3f2c8216e621929386
Jules Sandoz <jules.sandoz@heig-vd.ch>
2024-04-01 16:29:15 -0600

    Create BaseListItem component

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    'nuxt-icon'
+    'nuxt-icon',
+    '@nuxt/image',
+    '@nuxtjs/google-fonts',
   ],
-  css: ['~/assets/scss/main.scss']
+  css: ['~/assets/scss/main.scss', '~/assets/css/nuxt-google-fonts.css'],
+  googleFonts: {
+    families: {
+      'Josefin+Sans': true,
+      'Merriweather+Sans': [300, 400],
+    },
+    display: 'swap',
+    download: true,
+    outputDir: '~/assets/',
+  }
 
 })
