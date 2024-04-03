108b72c386c70afe7d87452f4f5e7983f9ff6cf4
Jules Sandoz <jules.sandoz@heig-vd.ch>
2024-04-03 11:09:52 -0600

    create BaseMediaSuggestion and BaseMediaSuggestionSkeleton components

---
+++ components/BaseMediaSuggestionSkeleton.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="suggestion-item">
+    <BaseSkeletonLoader height="180px" width="180px" />
+    <BaseSkeletonLoader height="1em" width="180px" />
+    <BaseSkeletonLoader height="1em" width="90px" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.suggestion-item {
+  padding: 15px 10px 0px 10px;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+}
+</style>
