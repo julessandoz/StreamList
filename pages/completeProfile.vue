c1b6ebd7ef9b0b87c0cb9ca799d15879f993879d
Jules Sandoz <jules.sandoz@heig-vd.ch>
2024-05-03 23:20:32 -0600

    create login/signup logic with pages login, confirm, and completeProfile

---
+++ pages/completeProfile.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>Welcome to StreamList</h1>
+  <p>
+    Now that you've created an account please complete your profile in order to use the
+    app!
+  </p>
+
+  <TheProfileUpdateForm />
+</template>
+
+<script setup lang="ts">
+const authStore = useAuthStore();
+const user = authStore.getUser;
+console.log(user);
+if (user.profile) {
+  navigateTo("/home");
+}
+</script>
