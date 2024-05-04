fc7cb9cfc344b8bdb1c8f47a004d154be4e06a3d
Jules Sandoz <jules.sandoz@heig-vd.ch>
2024-05-03 23:26:25 -0600

    add layouts

---
+++ layouts/landing.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <BaseButton
+      class="sign-in-btn"
+      text="Sign In"
+      size="sm"
+      btnStyle="filled"
+      type="button"
+      color="accent"
+    />
+  </div>
+  <slot />
+  <TheFooter />
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.sign-in-btn {
+  position: fixed;
+  top: 30px;
+  right: 30px;
+}
+</style>
