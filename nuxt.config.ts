9f10525304482086b84eac099a39bc2f6d7a9f3f
Jules Sandoz <jules.sandoz@heig-vd.ch>
2024-05-03 22:52:03 -0600

    add pinia and supabase modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,41 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: [
-    'nuxt-icon',
-    '@nuxt/image',
-    '@nuxtjs/google-fonts',
-  ],
-  css: ['~/assets/scss/main.scss', '~/assets/css/nuxt-google-fonts.css'],
+  runtimeConfig: {
+    public: {
+      appURL: process.env.APP_URL || 'http://localhost:3000'
+    }
+  },
+  supabase: {
+    redirect: false,
+    redirectOptions: {
+      login: '/landing',
+      callback: '/confirm',
+      include: undefined,
+      exclude: [],
+      cookieRedirect: false,
+    }
+  },
+  modules: ["nuxt-icon", "@nuxt/image", "@nuxtjs/google-fonts", '@pinia/nuxt', '@nuxtjs/supabase'],
+  css: ["~/assets/scss/main.scss", "~/assets/css/nuxt-google-fonts.css"],
   googleFonts: {
     families: {
-      'Josefin+Sans': true,
-      'Merriweather+Sans': [300, 400],
+      "Josefin+Sans": true,
+      "Merriweather+Sans": [300, 400],
     },
-    display: 'swap',
+    display: "swap",
     download: true,
-    outputDir: '~/assets/',
-  }
-
-})
+    outputDir: "~/assets/",
+  },
+  image: {
+    formats: ["webp", "avif"],
+    providers: {
+      tmdb: {
+        provider: "~/providers/tmdb.ts",
+        options: {
+          baseURL: "https://image.tmdb.org/t/p/original",
+        },
+      },
+    },
+  },
+});
